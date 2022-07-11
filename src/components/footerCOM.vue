22680c293bbe3bdf9a0044040f14d4571c3c87da
hafedh <hafedhdhouib@gmail.com>
2022-07-11 16:58:13 +0100

    valid

--- src/components/footerCOM.vue
+++ src/components/footerCOM.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="container">
-   <div class="row align-items-center d-inline">
-      <ul>
-         <li class="d-inline-block"><img class="a img-fluid" src="../assets/logo/association_baraa_.png"/></li>
-         <li class="d-inline-block"><img class=" img-fluid" src="../assets/logo/dd.jpg" height="170"/></li>
-<div class="d-inline ms-auto" >
-         <li class="d-inline-block"><img class="a img-fluid" src="../assets/logo/logo-AR-FR-DPE-.png"/></li>
-         <li class="d-inline-block"><img class="a img-fluid" src="../assets/logo/OIT_LOGO.jpg"/></li>
-         <li class="d-inline-block"><img class="a img-fluid" src="../assets/logo/wzara.jpg"/></li>
-     </div> </ul>       </div>
+    <div class="d-flex bd-highlight">
+  <div class="p-2 flex-grow-1 bd-highlight">      
+         <img class="a m-5 img-fluid" src="../assets/logo/association_baraa_.png"/>
+         <img class="a img-fluid" src="../assets/logo/protecte.jpg"/>
+</div>
+  <div class="p-2 bd-highlight"><img class="a m-5 img-fluid" src="../assets/logo/logo-AR-FR-DPE-.png"/>
+         <img class="a m-5 img-fluid" src="../assets/logo/OIT_LOGO.jpg"/>
+         <img class="a m-5 img-fluid" src="../assets/logo/wzara.jpg"/>
+</div>
+</div>
 
+<div class="b"></div>
+<div class="container">
     <footer class="py-5">
       <div class="float-start">
         <p>Follow us</p>
@@ -21,7 +23,7 @@
         </div>
       </div>
     </footer>
-  </div>
+    </div>
 </template>
 
 <script>
@@ -31,7 +33,7 @@ export default {};
 <style>
 @media screen  { 
 .a {
-  max-width: 200px;
+  max-width: 100px;
 }
 
 }
@@ -39,5 +41,9 @@ export default {};
 .a {
   max-width: 40px;
 }
+}
+.b{
+  height: 67px;
+  background-color:  #25389C;
 }
 </style>
