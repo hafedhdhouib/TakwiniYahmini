ae9f81d01bbc3e4206e2946e8c1bc410297583c9
hafedh <hafedhdhouib@gmail.com>
2022-07-11 17:00:34 +0100

    valide

--- src/components/footerCOM.vue
+++ src/components/footerCOM.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="d-flex bd-highlight">
   <div class="p-2 flex-grow-1 bd-highlight">      
-         <img class="a m-5 img-fluid" src="../assets/logo/association_baraa_.png"/>
-         <img class="a img-fluid" src="../assets/logo/protecte.jpg"/>
+         <img class="a m-5 m-md-2 img-fluid" src="../assets/logo/association_baraa_.png"/>
+         <img class="a m-md-2 img-fluid" src="../assets/logo/protecte.jpg"/>
 </div>
-  <div class="p-2 bd-highlight"><img class="a m-5 img-fluid" src="../assets/logo/logo-AR-FR-DPE-.png"/>
-         <img class="a m-5 img-fluid" src="../assets/logo/OIT_LOGO.jpg"/>
-         <img class="a m-5 img-fluid" src="../assets/logo/wzara.jpg"/>
+  <div class="p-2 bd-highlight"><img class="a m-5  img-fluid" src="../assets/logo/logo-AR-FR-DPE-.png"/>
+         <img class="a m-5 m-md-2 img-fluid" src="../assets/logo/OIT_LOGO.jpg"/>
+         <img class="a m-5 m-md-2 img-fluid" src="../assets/logo/wzara.jpg"/>
 </div>
 </div>
 
